3f9087b5bce871d64f7f186a9cf06831b7c8c68c
Fujh-nk <2217371905@qq.com>
2022-03-22 19:21:53 +0800

    Realized a simple TCP connection

--- Client/src/ftpclient.py
+++ Client/src/ftpclient.py
@@ -9,14 +9,10 @@ if __name__ == '__main__':
     host = socket.gethostname()
     port = 6666
     try:
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error:
         print("Failed to create socket!")
         sys.exit()
-    server_socket.bind((host, port))
-    server_socket.listen()
-    while True:
-        conn, addr = server_socket.accept()
-        print(conn, addr)
-        conn.close()
+    client_socket.connect((host, port))
+    client_socket.close()
     pass
--- Client/src/main.py
+++ Client/src/main.py
@@ -1,18 +1,4 @@
-import socket
-import struct
-import pickle
-import sys
-import time
-import os
+
 
 if __name__ == '__main__':
-    host = socket.gethostname()
-    port = 6666
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error:
-        print("Failed to create socket!")
-        sys.exit()
-    client_socket.connect((host, port))
-    client_socket.close()
     pass
