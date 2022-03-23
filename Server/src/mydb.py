2f4eb65abf87b339bbdfa89b0e676d5189acf501
Fujh-nk <2217371905@qq.com>
2022-03-23 14:55:19 +0800

    update for database(using sqlite) op

--- Server/src/mydb.py
+++ Server/src/mydb.py
@@ -1,11 +1,88 @@
 import sqlite3
 from dbutils.pooled_db import PooledDB
 
+config = {
+    'database': '../MyFtps.db',
+    'maxconnections': 0,
+    'mincached': 5,
+    'maxcached': 0,
+    'maxusage': None,
+    'blocking': True
+}
 
-class MyPooledDB:
+CREATE_TABLE_SQL = '''
+            CREATE TABLE IF NOT EXISTS USERS (
+            USERNAME VARCHAR (20) PRIMARY KEY,
+            PASSWORD VARCHAR (200) NOT NULL,
+            CANCELLED BOOLEAN DEFAULT FALSE,
+            PERMISSION INTEGER DEFAULT 0)
+            '''
+
+
+class MyDBConn:
+    __dbpool = None
+
     def __init__(self):
-        pass
+        self.conn = MyDBConn.get_connect()
+        self.cursor = self.conn.cursor()
+
+    @staticmethod
+    def get_connect():
+        """
+        get connection from database pool
+        :return: a connection to db
+        """
+        if MyDBConn.__dbpool is None:
+            MyDBConn.__dbpool = PooledDB(sqlite3, **config)
+        return MyDBConn.__dbpool.connection()
+
+    def update(self, sql, params):
+        """
+        insert, update or delete info from database
+        :param sql: the sql used for update
+        :param params: the params used for update
+        :return: a bool of update operator is executed or not
+        """
+        flag = False
+        try:
+            self.cursor.execute(sql, params)
+            self.conn.commit()
+            flag = True
+        except sqlite3.IntegrityError:
+            self.conn.rollback()
+            # add error log
+        return flag
+
+    def select(self, sql, params):
+        """
+        select from db with sql and params
+        :param sql: the sql used for select
+        :param params: the params for select
+        :return: result tuple
+        """
+        result = []
+        try:
+            self.cursor.execute(sql, params)
+            result = self.cursor.fetchall()
+        except sqlite3.OperationalError:
+            # add error log
+            pass
+        return tuple(result)
+
+    def release(self):
+        """
+        release database connection and cursor
+        :return: None
+        """
+        self.cursor.close()
+        self.conn.close()
 
 
 if __name__ == '__main__':
+    conn = MyDBConn()
+    conn.update("INSERT INTO USERS (USERNAME, PASSWORD) VALUES (?, ?)", ('test', '123456'))
+    print(conn.select("SELECT * FROM USERS WHERE USERNAME = ? AND PASSWORD = ?", ('test', '123456')))
+    conn.update("DELETE FROM USERS WHERE USERNAME = ?", ('test', ))
+    print(conn.select("SELECT * FROM USERS WHERE USERNAME = ? AND PASSWORD = ?", ('test', '123456')))
+    conn.release()
     pass
---
+++ Server/src/userdbop.py
@@ -0,0 +1,122 @@
+from mydb import MyDBConn
+from werkzeug.security import generate_password_hash, check_password_hash
+
+HASH_METHOD = 'pbkdf2:sha256'
+SALT_LENGTH = 8
+
+STATUS_OK = 0
+STATUS_FAILED = 1
+STATUS_NOT_EXIST = 11
+STATUS_WRONG_PASSWD = 12
+STATUS_USER_EXISTED = 13
+STATUS_USER_CANCELED = 14
+
+
+def exists(username):
+    """
+    select table to check if exists user called username or not
+    :param username: username for log in
+    :return: result tuple and connection to database
+    """
+    conn = MyDBConn()
+    user = conn.select("SELECT * FROM USERS WHERE USERNAME = ?", (username, ))
+    return user, conn
+
+
+def add_user(username, password):
+    """
+    try to add user to database, and need hash user's password for encrypt
+    :param username: username for log in
+    :param password: password for log in
+    :return: op status
+    """
+    user, conn = exists(username)
+    try:
+        if len(user) != 0:
+            return STATUS_USER_EXISTED
+        hash_passwd = generate_password_hash(password, HASH_METHOD, SALT_LENGTH)[len(HASH_METHOD) + 1:]
+        if conn.update("INSERT INTO USERS (USERNAME, PASSWORD) VALUES (?, ?)", (username, hash_passwd)):
+            return STATUS_OK
+        return STATUS_FAILED
+    finally:
+        conn.release()
+
+
+def check_user_passwd(username, password):
+    """
+    check user's password is correct or not
+    :param username: username for log in
+    :param password: password for log in
+    :return: op status
+    """
+    user, conn = exists(username)
+    try:
+        if len(user) == 0:
+            return STATUS_NOT_EXIST
+        if user[0][2]:
+            return STATUS_USER_CANCELED
+        hash_passwd = HASH_METHOD + ':' + user[0][1]
+        if check_password_hash(hash_passwd, password):
+            return STATUS_OK
+        return STATUS_WRONG_PASSWD
+    finally:
+        conn.release()
+
+
+def cancel_user(username):
+    """
+    cancel user's account
+    :param username: username for log in
+    :return: op status
+    """
+    user, conn = exists(username)
+    try:
+        if len(user) == 0:
+            return STATUS_NOT_EXIST
+        if conn.update("UPDATE USERS SET CANCELLED = ? WHERE USERNAME = ?", (True, username)):
+            return STATUS_OK
+        return STATUS_FAILED
+    finally:
+        conn.release()
+
+
+def update_user_permission(username, privilege):
+    """
+    update user's permission for download speed with privilege level
+    :param username: username for log in
+    :param privilege: the privilege want to set
+    :return: op status
+    """
+    user, conn = exists(username)
+    try:
+        if len(user) == 0 or user[0][2]:
+            return STATUS_NOT_EXIST
+        if conn.update("UPDATE USERS SET PERMISSION = ? WHERE USERNAME = ?", (privilege, username)):
+            return STATUS_OK
+        return STATUS_FAILED
+    finally:
+        conn.release()
+
+
+if __name__ == '__main__':
+    '''
+    passwd = generate_password_hash('password', method='pbkdf2:sha256', salt_length=8)
+    print(passwd[len(HASH_METHOD) + 1:])
+    print(passwd[:14] + passwd[20:])
+    result = check_password_hash(passwd, 'password')
+    print(result)
+    '''
+    print(add_user('test', '123456'))
+    conn = MyDBConn()
+    print(conn.select("SELECT * FROM USERS WHERE USERNAME = ?", ('test', )))
+    print(update_user_permission('test', 10))
+    print(conn.select("SELECT * FROM USERS WHERE USERNAME = ?", ('test', )))
+    print(check_user_passwd('test', '123456'))
+    print(check_user_passwd('test', '12346'))
+    print(cancel_user('test'))
+    print(conn.select("SELECT * FROM USERS WHERE USERNAME = ?", ('test', )))
+    print(check_user_passwd('test', '123456'))
+    print(check_user_passwd('test', '12346'))
+    # conn.update("DELETE FROM USERS WHERE USERNAME = ?", ('test',))
+    conn.release()
+
